8f616e574b77693ff200257c9bfb637f06ccf2fc
John Galindez <jgalindez@chanzglobal.com>
2025-02-09 20:07:29 +0800

    Added migration folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "sqlite",
   schema: "./server/db/schema.ts",
+  out: "./server/db/migrations", // Set where migrations are stored
 });
