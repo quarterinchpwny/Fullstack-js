7f549adc5bf6d9e1540c3b6e0115f547a217b995
John Francis Agustin Galindez <jgalindez@chanzglobal.com>
2025-03-10 18:01:22 +0800

    saved progress

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
 import { logger } from "hono/logger";
-import { expensesRoute } from "./routes/expenses";
+import { transactionsRoute } from "./routes/expenses";
 import { categoriesRoute } from "./routes/categories";
 
 const app = new Hono();
@@ -13,7 +13,7 @@ app.get("*", serveStatic({ root: "./frontend/dist/index.html" }));
 // API route
 const apiRoutes = app
   .basePath("/api")
-  .route("/expenses", expensesRoute)
+  .route("/expenses", transactionsRoute)
   .route("/categories", categoriesRoute);
 
 export default app;
