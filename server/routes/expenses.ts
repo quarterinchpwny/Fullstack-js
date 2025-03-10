e6e99c953dfcbebb9a72b3bf6c932c8447811909
John Francis Agustin Galindez <jgalindez@chanzglobal.com>
2025-03-10 17:58:37 +0800

    saved progress

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -1,62 +1,80 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { db } from "../db";
-import { expenses as expenseTable } from "../db/schema/expenses";
-import { insertExpensesSchema } from "../db/schema/expenses";
-import { eq, desc, sum, and, count } from "drizzle-orm";
-import { categories } from "../db/schema/categories";
+import { transactions as transactionTable } from "../db/schema/transactions";
+import { insertTransactionSchema } from "../db/schema/transactions";
+import { eq, desc, sum, and, count, is } from "drizzle-orm";
+import { categories as categoryTable } from "../db/schema/categories";
+import { transactionTypes as transactionTypeTable } from "../db/schema/transaction_type";
 
-const postSchema = insertExpensesSchema.omit({
+const postSchema = insertTransactionSchema.omit({
   createdAt: true,
   id: true,
 });
 
-export const expensesRoute = new Hono()
+export const transactionsRoute = new Hono()
   .get("/", async (c) => {
     try {
       const data = await db
         .select({
-          id: expenseTable.id,
-          amount: expenseTable.amount,
-          title: expenseTable.title,
-          description: expenseTable.description,
-          createdAt: expenseTable.createdAt,
+          id: transactionTable.id,
+          amount: transactionTable.amount,
+          title: transactionTable.title,
+          description: transactionTable.description,
+          createdAt: transactionTable.createdAt,
+          isRecurring: transactionTable.isRecurring,
+          recurringFrequency: transactionTable.recurringFrequency,
+          nextDueDate: transactionTable.nextDueData,
           category: {
-            id: expenseTable.categoryId,
-            name: categories.name,
+            id: transactionTable.categoryId,
+            name: categoryTable.name,
+          },
+          transactionType: {
+            id: transactionTable.transationTypeId,
+            name: transactionTypeTable.name,
           },
         })
-        .from(expenseTable)
-        .leftJoin(categories, eq(expenseTable.categoryId, categories.id));
-      return c.json({ expenses: data, success: true });
+        .from(transactionTable)
+        .leftJoin(
+          categoryTable,
+          eq(transactionTable.categoryId, categoryTable.id)
+        )
+        .leftJoin(
+          transactionTypeTable,
+          eq(transactionTable.transationTypeId, transactionTypeTable.id)
+        );
+      return c.json({ transactions: data, success: true });
     } catch (error) {
-      return c.json({ error: "Failed to fetch expenses", success: false }, 500);
+      return c.json(
+        { error: "Failed to fetch transactions", success: false },
+        500
+      );
     }
   })
   .post("/", zValidator("json", postSchema), async (c) => {
     try {
-      const expense = c.req.valid("json");
-      if (expense.categoryId) {
+      const transaction = c.req.valid("json");
+      if (transaction.categoryId) {
         const category = await db
           .select()
-          .from(categories)
-          .where(eq(categories.id, expense.categoryId))
+          .from(categoryTable)
+          .where(eq(categoryTable.id, transaction.categoryId))
           .limit(1);
         if (category.length === 0) {
           return c.json({ error: "Category not found", success: false }, 400);
         }
       }
       const result = await db
-        .insert(expenseTable)
+        .insert(transactionTable)
         .values({
-          ...expense,
+          ...transaction,
         })
         .returning();
       return c.json({ result, success: true }, 201);
     } catch (error) {
       return c.json(
         {
-          error: "Failed to create expense",
+          error: "Failed to create transaction",
           success: false,
         },
         500
@@ -65,14 +83,16 @@ export const expensesRoute = new Hono()
   })
   .get("/total-spent", async (c) => {
     const result = await db
-      .select({ total: sum(expenseTable.amount) })
-      .from(expenseTable)
+      .select({ total: sum(transactionTable.amount) })
+      .from(transactionTable)
       .limit(1)
       .then((res) => res[0]);
-    const countResult = await db.select({ count: count() }).from(expenseTable);
+    const countResult = await db
+      .select({ count: count() })
+      .from(transactionTable);
     return c.json({
       totalSpent: result.total,
-      expensesCount: countResult[0].count,
+      transactionsCount: countResult[0].count,
     });
   })
 
@@ -81,12 +101,12 @@ export const expensesRoute = new Hono()
     if (Number.isNaN(id)) {
       return c.notFound();
     }
-    const expense = db
+    const transaction = db
       .select()
-      .from(expenseTable)
-      .where(eq(expenseTable.id, id));
-    if (expense) {
-      return c.json({ expense: expense });
+      .from(transactionTable)
+      .where(eq(transactionTable.id, id));
+    if (transaction) {
+      return c.json({ transaction: transaction });
     }
     return c.notFound();
   })
@@ -96,15 +116,18 @@ export const expensesRoute = new Hono()
       return c.notFound();
     }
     try {
-      const expense = await db
-        .delete(expenseTable)
-        .where(eq(expenseTable.id, id))
+      const transaction = await db
+        .delete(transactionTable)
+        .where(eq(transactionTable.id, id))
         .returning();
-      if (expense.length === 0) {
+      if (transaction.length === 0) {
         return c.notFound();
       }
-      return c.json({ expense: expense[0], success: true });
+      return c.json({ transaction: transaction[0], success: true });
     } catch (error) {
-      return c.json({ error: "Failed to delete expense", success: false }, 500);
+      return c.json(
+        { error: "Failed to delete transaction", success: false },
+        500
+      );
     }
   });
